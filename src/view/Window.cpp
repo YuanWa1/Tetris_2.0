73176cb92a59d9b8cc5703a84cb9affcacb3ca55
pawan <justpawan7@gmail.com>
2026-01-21 21:18:37 -0600

    Revert "fixed the pointer issue"
    This reverts commit 3129d3e6d6f7ccc633d6e61c97b27016620709a7.

--- src/view/Window.cpp
+++ src/view/Window.cpp
@@ -42,8 +42,9 @@ Window::Window(int width, int height, const char* appName) {
     //windowed fallback, you can do:
     window = glfwCreateWindow(width, height, appName, nullptr, nullptr);
 
+    std::cout << "Window created"  << this << std::endl;
     // give opengl a reference to current class
-
+    glfwSetWindowUserPointer(window, this);
 
 
 
@@ -104,9 +105,7 @@ void Window::framebuffer_size_callback(GLFWwindow* window, int width, int height
     glViewport(0, 0, width, height);
     auto* self = static_cast<Window*>(glfwGetWindowUserPointer(window));
     if (!self) return;
-    std::cout << "callback GLFWwindow* @ " << (void*)window
-          << " | userptr self @ " << (void*)self << "\n";
-
+    std::cout << "window" << self <<  std::endl;
     self->currWidth = width;
     self->currHeight = height;
 
