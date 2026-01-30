5334ebd6d9c8b2f3f5969d0b30d6066425fa8c00
pawan subedi <justpawan7@gmail.com>
2026-01-29 19:49:15 -0600

    classes

---
+++ src/model/Pieces.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pawan on 1/29/26.
+//
+
+#include "Pieces.h"
---
+++ src/model/Pieces.h
@@ -0,0 +1,14 @@
+//
+// Created by Pawan on 1/29/26.
+//
+
+#ifndef TETRIS_2_0_PIECES_H
+#define TETRIS_2_0_PIECES_H
+
+
+class Pieces
+{
+};
+
+
+#endif //TETRIS_2_0_PIECES_H
